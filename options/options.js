bbc11e8bfa7163dcce74fd2ebb768e040abc115e
Crowdbotics <team@crowdbotics.com>
2022-08-08 14:53:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nosolo_halo_app_36666",
+  url: "https://nosolo_halo_app_36666.botics.co",
+  api: "https://nosolo_halo_app_36666.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
